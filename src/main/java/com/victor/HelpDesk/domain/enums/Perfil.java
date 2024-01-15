ba4398843c190f4cbc006600c6264100616f5a3a
vihjoulle <victordeghandi@gmail.com>
2024-01-15 12:02:14 -0300

    tipos enumerados criados

---
+++ src/main/java/com/victor/HelpDesk/domain/enums/Perfil.java
@@ -0,0 +1,37 @@
+package com.victor.HelpDesk.domain.enums;
+
+@SuppressWarnings({"unused", "UnnecessaryModifier"})
+public enum Perfil {
+
+ ADMIN (0, "ROLE_ADMIN"), CLIENTE (1, "ROLE_CLIENTE"), TECNICO (2, "ROLE_TECNICO");
+
+    private final int codigo;
+    private final String descricao;
+
+    private Perfil(int codigo, String descricao) {
+        this.codigo = codigo;
+        this.descricao = descricao;
+    }
+
+    public int getCodigo() {
+        return codigo;
+    }
+
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public static Perfil toEnum(Integer cod) throws IllegalAccessException {
+        if(cod == null) {
+            return null;
+        }
+        for (Perfil x: Perfil.values()){
+            if(cod.equals(x.getCodigo())){
+                return x;
+            }
+        }
+        throw new IllegalAccessException("Perfil inválido");
+    }
+}
+
+
